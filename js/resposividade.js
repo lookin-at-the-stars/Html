544a1351524e9a350eecb1cdcd08ee197dba9b4e
Enzosophi <enzo.oliveira05@gmail.com>
2024-10-15 01:36:48 -0300

    Código de Responsividade da nav Hamburger e inicio do corpo da home

---
+++ js/resposividade.js
@@ -0,0 +1,27 @@
+const navbar = document.querySelector(".navbar");
+const menuButton = document.querySelector(".menu-btn");
+const menu = document.querySelector("nav");
+const closeButton = document.querySelector(".close-btn");
+
+// Adiciona o evento de clique no botão de menu
+menuButton.addEventListener("click", () => {
+    menu.style.display = menu.style.display === "block" ? "none" : "block";
+    closeButton.style.display = menu.style.display === "block" ? "block" : "none"; // Mostra/oculta o "X"
+});
+
+// Adiciona evento de clique para fechar o menu
+closeButton.addEventListener("click", () => {
+    menu.style.display = "none"; // Oculta o menu
+    closeButton.style.display = "none"; // Esconde o "X"
+});
+
+// Adiciona evento para redimensionamento da janela
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 800) {
+        menu.style.display = "flex"; // Exibe o menu em telas maiores
+        closeButton.style.display = "none"; // Esconde o "X" em telas maiores
+    } else {
+        menu.style.display = "none"; // Oculta o menu em telas menores
+        closeButton.style.display = "none"; // Esconde o "X"
+    }
+});
